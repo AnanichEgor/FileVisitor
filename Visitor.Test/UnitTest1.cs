3067c427835aeecb7533dba0c3e4c234c3d6418b
Yahor Ananich <Yahor_Ananich@epam.com>
2019-03-22 15:34:13 +0300

    add Test empty project

---
+++ Visitor.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Visitor.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

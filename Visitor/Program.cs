dc2f5f2ec65d011d15c6e0521db1d102384b62de
Yahor Ananich <Yahor_Ananich@epam.com>
2019-03-22 15:33:54 +0300

    add run example

--- Visitor/Program.cs
+++ Visitor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Visitor
 {
@@ -7,7 +8,50 @@ namespace Visitor
         static void Main(string[] args)
         {
             var a = new FileSystemVisitor("d:\\");
+            a.Start += WriteToConsole;
+            a.Finish += WriteToConsole;
+            a.FileFinded += EventSearch;
+            a.DirectoryFinded += EventSearch;
+            a.FilteredFileFinded += EventFilter;
+            a.FilteredDirectoryFinded += EventFilter;
+            a.Execute();
+
+            //var b = new FileSystemVisitor("d:\\", (fileName) => { return fileName .Length > 10; });
+            //b.Execute();
+
+            //var c = new FileSystemVisitor("d:\\", Fits, "a*.*");
+            //c.Execute();
+
+            var resultA = a.GetFiles();
+            //var resultB = b.GetFiles();
+            //var resultC = c.GetFiles();
             Console.ReadKey();
+        }
+
+        public static bool Fits(string sFileName, Regex sFileMaskRegex)
+        {
+            return sFileMaskRegex.IsMatch(sFileName);
+        }
+
+        public static void WriteToConsole(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static bool EventSearch(string message)
+        {
+            Console.WriteLine(message);
+            // then write the desired implementation
+            // if return "true" filtering will abort
+            return false;
+        }
+
+        public static bool EventFilter(string message)
+        {
+            Console.WriteLine(message);
+            // then write the desired implementation
+            // if return "true" filtering will abort
+            return false;
         }
     }
 }

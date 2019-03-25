b22e08532e3bf15bd5b6bb9637f4c29e676c2596
Yahor Ananich <Yahor_Ananich@epam.com>
2019-03-25 17:39:32 +0300

    add interface IDirectoryInfo

---
+++ Visitor/IDirectoryInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace Visitor
+{
+    public interface IDirectoryInfo
+    {
+        bool Exists { get; }
+        string FullName { get; }
+        IDirectoryInfo[] GetDirectories();
+        FileInfo[] GetFiles();
+        DateTime CreationTime { get; set; }
+    }
+}
